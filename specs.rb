b33a1e3b260db7e6a33683a770a5dc6807195419
wolfdancer <wolfdancer@ed7eda33-4b9a-41fc-af1f-e75b3c93d7ee>
2006-12-28 07:30:27 +0000

    setting up project
    git-svn-id: http://selenium.rubyforge.org/svn/trunk@4 ed7eda33-4b9a-41fc-af1f-e75b3c93d7ee

---
+++ specs.rb
@@ -0,0 +1,31 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'rubygems'
+Gem::manage_gems
+require 'rake'
+require 'buildmaster/project'
+require 'buildmaster/site'
+require 'buildmaster/cotta'
+
+SITE_SPEC = BuildMaster::SiteSpec.new(__FILE__) do |spec|
+  spec.content_dir = 'site'
+  spec.output_dir = 'build/website/selenium'
+  spec.template_file = 'site_template.html'
+end
+
+VERSION_NUMBER = BuildMaster::VersionNumberFile.new(BuildMaster::Cotta.parent_of(__FILE__).file('lib/selenium/version'))
+
+SPEC = Gem::Specification.new do |spec|
+  spec.name = 'Selenium'
+  spec.version = VERSION_NUMBER.version_number
+  spec.author = 'Shane Duan'
+  spec.email = 'selenium@gmail.com'
+  spec.homepage = 'http://selenium.rubyforge.org/'
+  spec.platform = Gem::Platform::RUBY
+  spec.summary = 'A project that wraps selenium API into object-oriented testing style and packages it into a RubyGem.'
+  spec.files = FileList["{bin,docs,lib,test}/**/*"].exclude("rdoc").to_a
+  spec.require_path = 'lib'
+  spec.autorequire = 'selenium'
+  spec.has_rdoc = true
+  spec.extra_rdoc_files = ["README"]
+end
