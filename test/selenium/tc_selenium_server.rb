e8016c9be73dce89f35aa2e79f167de79be9457a
wolfdancer <wolfdancer@ed7eda33-4b9a-41fc-af1f-e75b3c93d7ee>
2006-12-29 07:55:29 +0000

    selenium server created
    git-svn-id: http://selenium.rubyforge.org/svn/trunk@7 ed7eda33-4b9a-41fc-af1f-e75b3c93d7ee

---
+++ test/selenium/tc_selenium_server.rb
@@ -0,0 +1,20 @@
+require 'spec'
+require 'buildmaster/cotta'
+require 'buildmaster/project/server_manager'
+
+$:.unshift File.join(File.dirname(__FILE__), '..', '..', 'lib', 'selenium')
+
+require 'selenium_server'
+
+module Selenium
+context 'selenium server' do
+  specify 'start and stop server' do
+    cotta = BuildMaster::Cotta.new
+    server = BuildMaster::ServerManager.new(SeleniumServer.new(cotta))
+    puts 'starting server...'
+    server.start
+    puts 'stopping server...'
+    server.stop
+  end
+end
+end
