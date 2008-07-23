8a91412ef3c79aeb44af6ec5e30b752b4d594c03
Brian Takita <brian.takita@gmail.com>
2008-07-22 22:53:24 -0700

    All but one test pass.

--- lib/selenium/button.rb
+++ lib/selenium/button.rb
@@ -1,4 +1,7 @@
 module Selenium
   class Button < HtmlElement
+    def click_wait
+      webpage.click_wait locator
+    end
   end
 end
--- lib/selenium/html_element.rb
+++ lib/selenium/html_element.rb
@@ -4,5 +4,13 @@ module Selenium
     def initialize(webpage, locator)
       @webpage, @locator = webpage, locator
     end
+
+    def double_click
+      webpage.double_click locator
+    end
+
+    def key_press(key)
+      webpage.key_press locator, key
+    end
   end
 end
